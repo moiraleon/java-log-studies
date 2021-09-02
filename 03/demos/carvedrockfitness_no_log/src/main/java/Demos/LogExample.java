83be2474e7ae5afad160ba8d5743b45d1bd81234
Moira Leon <moira.leon2@gmail.com>
2021-09-02 16:36:42 -0600

    Log Levels

--- 03/demos/carvedrockfitness_no_log/src/main/java/Demos/LogExample.java
+++ 03/demos/carvedrockfitness_no_log/src/main/java/Demos/LogExample.java
@@ -9,3 +9,13 @@ public class LogExample {
        LOGGER.log(Level.INFO,"This is an example message");
     }
 }
+/*
+7 levels of Logger Levels based on severity
+ SEVERE,WARNING,INFO,CONFIG,FINE,FINER,FINEST
+
+
+ ...
+ LOGGER.setLevel(Level.FINEST);
+ LOGGER.log(Level.FINEST,"This is an example message");
+ int this example when run, the message will not appear in our terminal due to handlers
+*/
