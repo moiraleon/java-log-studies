5fdc0301e7fb5257d94b82155dbdd9f58943468b
Moira Leon <moira.leon2@gmail.com>
2021-09-02 16:52:08 -0600

    Best Practice and Different Logs

--- 03/demos/carvedrockfitness_no_log/src/main/java/Demos/LogExample.java
+++ 03/demos/carvedrockfitness_no_log/src/main/java/Demos/LogExample.java
@@ -40,3 +40,26 @@ Log Handler Notes
 Act as filters
 Different types: ConsoleHandler, FileHandler, StreamHandler, SocketHandler, MemoryHandler
  */
+
+/*
+Best Practices Notes
+if something is a warning don't log as severe
+if something is an error don't look with level info
+don't log too much or too little
+
+3 Log Methods
+
+
+LOGGER.log(Level.INFO, "This is an example message");
+
+regular log, specify level and give message
+
+LOGGER.logp(Level.INFO, LogExample.class.getName(), "sourceMethodName", ""This is an example message");
+
+log precise, specify level and class from which log message is derived, and method, and message
+*best practice
+
+LOGGER.logrb(Level.INFO, ResourceBundle.getBundle("en_US"), "This is an example message");
+
+log resource bundle, great for localization
+ */
